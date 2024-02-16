a311f8f1d3be2c7067c5e0640f37ee147b89087b
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-02-16 11:25:16 +0000

    Added real time of current location.

--- javascript/index.js
+++ javascript/index.js
@@ -35,6 +35,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityDate = moment().tz(cityTimeZone).format("MMMM Do YYYY");
   let cityTime = moment()
